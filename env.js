2a54b7e6e517202ffec3366d4be4f955b067cc1e
CrystalBlockDev <crytal.blockdev@gmail.com>
2022-08-03 11:04:58 +0900

    init

---
+++ env.js
@@ -0,0 +1,12 @@
+const PRIVATE_KEY = ''
+
+const TOKEN_ADDRESS = '0x55d398326f99059fF775485246999027B3197955'; // USDT address
+const AMOUNT = 1
+const LEVEL = 3
+
+module.exports = {
+    PRIVATE_KEY,
+    TOKEN_ADDRESS,
+    AMOUNT,
+    LEVEL
+};
